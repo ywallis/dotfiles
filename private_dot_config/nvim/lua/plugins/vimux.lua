15a990fbbce6e71967f6c30ea28180787d40bda3
ywallis <yann.wallis@gmail.com>
2025-02-25 15:17:56 +0800

    added ignore

--- private_dot_config/nvim/lua/plugins/vimux.lua
+++ private_dot_config/nvim/lua/plugins/vimux.lua
@@ -9,11 +9,18 @@ return {
 				":call VimuxRunCommandInDir('uv run', 1)<CR>",
 				{ noremap = true, silent = true }
 			)
+			vim.keymap.set("n", "<leader>um", function()
+				local file_path = vim.fn.expand("%:p:r") -- Get full path without extension
+				local cwd = vim.fn.getcwd() -- Get the current working directory
+				local rel_path = file_path:gsub(cwd .. "/", "") -- Remove the working directory part
+				local module_path = rel_path:gsub("/", ".") -- Convert path to module format
+
+				local command = "uv run -m " .. module_path -- Final command
+				vim.fn.VimuxRunCommand(command) -- Execute in Vimux
+			end, { noremap = true, silent = true })
 			-- vim.api.nvim_set_keymap("n", "<leader>cr", ":call VimuxRunCommandInDir('cargo run', 1)<CR>", { noremap = true, silent = true })
-			vim.api.nvim_set_keymap("n", "<leader>vq", ":VimuxCloseRunner<CR>",
-				{ noremap = true, silent = true })
-			vim.api.nvim_set_keymap("n", "<leader>vx", ":VimuxInterruptRunner<CR>",
-				{ noremap = true, silent = true })
+			vim.api.nvim_set_keymap("n", "<leader>vq", ":VimuxCloseRunner<CR>", { noremap = true, silent = true })
+			vim.api.nvim_set_keymap("n", "<leader>vx", ":VimuxInterruptRunner<CR>", { noremap = true, silent = true })
 		end,
 	},
 	{
