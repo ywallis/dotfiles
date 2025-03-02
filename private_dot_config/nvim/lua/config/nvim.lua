f63ad857973108a943011167dfc6111998080fa0
ywallis <yann.wallis@gmail.com>
2025-03-02 10:53:48 +0800

    bunch of fixes, including the notorious xclip one

--- private_dot_config/nvim/lua/config/nvim.lua
+++ private_dot_config/nvim/lua/config/nvim.lua
@@ -2,3 +2,9 @@ vim.opt.clipboard = "unnamedplus"
 vim.opt.number = true
 vim.api.nvim_set_keymap("t", "<Esc>", "<C-\\><C-n>", { noremap = true, silent = true })
 vim.opt.relativenumber = true
+
+local session_file = vim.fn.getcwd() .. "/Session.vim"
+if vim.fn.filereadable(session_file) == 1 then
+  vim.cmd("source " .. session_file)
+end
+
