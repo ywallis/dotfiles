cfb7993d50e4e2486b474033e39505862dc9662e
ywallis <yann.wallis@gmail.com>
2025-06-11 08:35:45 +0200

    added ignoring of nvim session when opening git messages and added bat (iso cat)

--- private_dot_config/nvim/lua/config/nvim.lua
+++ private_dot_config/nvim/lua/config/nvim.lua
@@ -3,8 +3,13 @@ vim.opt.number = true
 vim.api.nvim_set_keymap("t", "<Esc>", "<C-\\><C-n>", { noremap = true, silent = true })
 vim.opt.relativenumber = true
 
-local session_file = vim.fn.getcwd() .. "/Session.vim"
-if vim.fn.filereadable(session_file) == 1 then
-  vim.cmd("source " .. session_file)
-end
+local argv = vim.fn.argv()
+local file = argv[1] or ""
+local is_git_edit = file:match("COMMIT_EDITMSG") or file:match("MERGE_MSG") or file:match("git%-rebase%-todo")
 
+if not is_git_edit then
+  local session_file = vim.fn.getcwd() .. "/Session.vim"
+  if vim.fn.filereadable(session_file) == 1 then
+    vim.cmd("source " .. session_file)
+  end
+end
