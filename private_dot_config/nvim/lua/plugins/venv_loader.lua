67f2153c9e54e41ff5cf6c4ec69a6a021fa86c3e
ywallis <yann.wallis@gmail.com>
2025-07-31 12:31:02 +0200

    Changed env loader for python

--- private_dot_config/nvim/lua/plugins/venv_loader.lua
+++ private_dot_config/nvim/lua/plugins/venv_loader.lua
@@ -1,4 +1,5 @@
 return {
+
 	"linux-cultist/venv-selector.nvim",
 	dependencies = {
 		"neovim/nvim-lspconfig",
@@ -8,11 +9,11 @@ return {
 	},
 	lazy = false,
 	branch = "regexp", -- This is the regexp branch, use this for the new version
-	config = function()
-		require("venv-selector").setup()
-	end,
 	keys = {
-		-- { ",v", "<cmd>VenvSelect<cr>" },
 		{ "<leader>vs", "<cmd>VenvSelect<cr>" },
+	},
+	---@type venv-selector.Config
+	opts = {
+		-- Your settings go here
 	},
 }
