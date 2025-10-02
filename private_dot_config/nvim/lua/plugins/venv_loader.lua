89d8ae329d3ae269f6e9ada89f79e86e99dca273
ywallis <yann.wallis@gmail.com>
2025-10-02 18:03:24 +0200

    fix venv loader

--- private_dot_config/nvim/lua/plugins/venv_loader.lua
+++ private_dot_config/nvim/lua/plugins/venv_loader.lua
@@ -1,19 +1,15 @@
 return {
-
 	"linux-cultist/venv-selector.nvim",
 	dependencies = {
 		"neovim/nvim-lspconfig",
-		"mfussenegger/nvim-dap",
-		"mfussenegger/nvim-dap-python", --optional
-		{ "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+		{ "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } }, -- optional: you can also use fzf-lua, snacks, mini-pick instead.
 	},
-	lazy = false,
-	branch = "main",
+	ft = "python",                                                                               -- Load when opening Python files
 	keys = {
 		{ "<leader>vs", "<cmd>VenvSelect<cr>" },
 	},
-	---@type venv-selector.Config
-	opts = {
-		-- Your settings go here
+	opts = { -- this can be an empty lua table - just showing below for clarity.
+		search = {}, -- if you add your own searches, they go here.
+		options = {} -- if you add plugin options, they go here.
 	},
 }
