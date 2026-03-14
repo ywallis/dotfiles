df26a196b3278b825b83ef0b616aebadc921954c
Yann Wallis <yann.wallis@gmail.com>
2026-03-14 08:38:41 +0100

    removed provider option in opencode

--- private_dot_config/nvim/lua/plugins/opencode-nvim.lua
+++ private_dot_config/nvim/lua/plugins/opencode-nvim.lua
@@ -31,12 +31,6 @@ return {
 	config = function()
 		---@type opencode.Opts
 		vim.g.opencode_opts = {
-			provider = {
-				enabled = "terminal",
-				terminal = {
-					-- ...
-				}
-			}
 		}
 		vim.o.autoread = true -- Required for `opts.events.reload`
 
