bd2e2fe5d993a559b34c003fcbf093367938120b
Yann Wallis <yann.wallis@gmail.com>
2026-03-23 11:40:59 +0100

    change case setting for telescope

--- private_dot_config/nvim/lua/plugins/telescope.lua
+++ private_dot_config/nvim/lua/plugins/telescope.lua
@@ -6,6 +6,13 @@ return {
 			-- optional but recommended
 			{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }, },
 		config = function()
+			require('telescope').setup {
+				extensions = {
+					fzf = {
+						case_mode = "smart_case", -- or "smart_case", "respect_case"
+					}
+				}
+			}
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
